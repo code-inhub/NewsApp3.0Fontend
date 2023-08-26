5cf47d31aaa86387a25ccf39cb834620903e64ce
Anshul Rana <anshulrana2510@gmail.com>
2023-08-26 19:36:57 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:8000';
+const url = 'https://newsapp3-0backend.onrender.com';
 
 export const CheckLogin = async ({email,password}) => {
     try{
